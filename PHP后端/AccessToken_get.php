8edb30324d68eaee375ca85946185e8e608cf6b6
草札 <8866629@qq.com>
2020-04-30 11:03:44 +0800

    1.0.0

--- PHP后端/AccessToken_get.php
+++ PHP后端/AccessToken_get.php
@@ -1,4 +1,17 @@
 <?php
+/*
+☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+☆                                                                         ☆
+☆  系 统：AI智能识物                                                        ☆
+☆  日 期：2019-07                                                          ☆
+☆  开 发：草札(www.caozha.com)                                              ☆
+☆  鸣 谢：穷店(www.qiongdian.com) 品络(www.pinluo.com)                      ☆
+☆  声 明: 使用本程序源码必须保留此版权声明等相关信息！                            ☆
+☆  Copyright ©2020 www.caozha.com All Rights Reserved.                    ☆
+☆                                                                         ☆
+☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+*/
+
 include 'config.php';
 echo json_encode(getAccessToken($client_id,$client_secret));
 
